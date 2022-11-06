605565ae1302c9e73cfee1d7928177a69f64bcf0
Thm-lab <1052251461@qq.com>
2022-11-06 17:38:27 +0800

    student get grade status render

--- mock/student-grade.js
+++ mock/student-grade.js
@@ -2,8 +2,6 @@ const Mock = require('mockjs')
 
 const data = Mock.mock({
     'grade|6': [{
-        Class: '@integer(1,15)',
-        Name: '@string( 2, 4)',
         Subject: '@string(2,3)',
         ObjectiveScore: '@integer(0,150)',
         SubjectiveScore: '@integer(0,150)',
@@ -11,6 +9,9 @@ const data = Mock.mock({
         SubjectRank: '@integer(0,100)',
         Rank: '@integer(0,1000)',
         Time: '@datetime',
+        ExamName: '@string(4)',
+        // 0: had not submit; 1: checking; 2: accepted; 3: rejected
+        "Status|0-3": 100
     }]
 })
 
--- src/views/student-get-grade/index.vue
+++ src/views/student-get-grade/index.vue
@@ -15,6 +15,24 @@
       <el-col :span="2"
         ><el-button icon="el-icon-search" circle></el-button
       ></el-col>
+      <el-col :span="1" :offset="16"
+        ><div class="block">
+          <span class="demonstration">查分中</span>
+          <el-color-picker v-model="checkingColor"></el-color-picker>
+        </div>
+      </el-col>
+      <el-col :span="1"
+        ><div class="block">
+          <span class="demonstration">已更正</span>
+          <el-color-picker v-model="acceptColor"></el-color-picker>
+        </div>
+      </el-col>
+      <el-col :span="1"
+        ><div class="block">
+          <span class="demonstration">驳回</span>
+          <el-color-picker v-model="rejectColor"></el-color-picker>
+        </div>
+      </el-col>
     </el-row>
     <el-table
       v-loading="gradeLoading"
@@ -23,19 +41,8 @@
       border
       fit
       highlight-current-row
+      :row-class-name="statusShow"
     >
-      <el-table-column align="center" label="班级">
-        <template slot-scope="scope">
-          {{ scope.row.Class }}
-        </template>
-      </el-table-column>
-
-      <el-table-column align="center" label="姓名">
-        <template slot-scope="scope">
-          {{ scope.row.Name }}
-        </template>
-      </el-table-column>
-
       <el-table-column label="科目" align="center">
         <template slot-scope="scope">
           <span>{{ scope.row.Subject }}</span>
@@ -74,6 +81,8 @@
 
       <el-table-column align="center" label="考试时间">
         <template slot-scope="scope">
+          <span>{{ scope.row.ExamName }}</span>
+          <br />
           <i class="el-icon-time" />
           <span>{{ scope.row.Time }}</span>
         </template>
@@ -91,6 +100,7 @@
 
     <el-dialog title="申请查分" :visible.sync="checkGradeForm" center>
       <el-form :model="subjects">
+        <br />
         <el-form-item label="查分科目" label-width="auto">
           <el-select v-model="selectedSubject" placeholder="请选择查分科目">
             <el-option
@@ -175,22 +185,15 @@
 import { getGrade } from "@/api/student";
 
 export default {
-  filters: {
-    statusFilter(status) {
-      const statusMap = {
-        published: "success",
-        draft: "gray",
-        deleted: "danger",
-      };
-      return statusMap[status];
-    },
-  },
   data() {
     return {
       grade: null,
       gradeLoading: true,
       exams: [],
       subjects: [],
+      checkingRowIndex: [],
+      acceptRowIndex: [],
+      rejectRowIndex: [],
       selected: "",
       searchInfo: "",
       checkGradeForm: false,
@@ -198,6 +201,9 @@ export default {
       selectedExam: null,
       checkQuestion: null,
       checkReason: "",
+      checkingColor: "#fdf5e6",
+      acceptColor: "#f0f9eb",
+      rejectColor: "#ff8985",
     };
   },
   created() {
@@ -209,15 +215,32 @@ export default {
       getGrade().then((response) => {
         this.grade = response.data.grade;
         this.gradeLoading = false;
-        this.grade.forEach((e) => {
-          this.exams.push({ value: e.Time, label: e.Time });
+        this.grade.forEach((e, i) => {
+          this.exams.push({ value: e.Time, label: e.ExamName });
           this.subjects.push(e.Subject);
+          if (e.Status === 1) {
+            this.checkingRowIndex.push(i);
+          } else if (e.Status === 2) {
+            this.acceptRowIndex.push(i);
+          } else if (e.Status === 3) {
+            this.rejectRowIndex.push(i);
+          }
         });
         // duplicate removal
         this.subjects = this.subjects.filter(
           (item, index) => this.subjects.indexOf(item) === index
         );
       });
+    },
+    statusShow({ row, rowIndex }) {
+      if (this.checkingRowIndex.includes(rowIndex)) {
+        return "checking-row";
+      } else if (this.acceptRowIndex.includes(rowIndex)) {
+        return "accept-row";
+      } else if (this.rejectRowIndex.includes(rowIndex)) {
+        return "reject-row";
+      }
+      return "";
     },
   },
 };
@@ -237,6 +260,16 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+}
+.el-table .checking-row {
+  background: oldlace;
+}
+
+.el-table .accept-row {
+  background: #f0f9eb;
+}
+.el-table .reject-row {
+  background: #ff8985;
 }
 .el-dialog {
   display: flex;
